e6ea0afea57e9d253e7a0ada428bc0b7ffac93e7
Daniel Ye <danielyeqt@gmail.com>
2021-02-23 12:23:58 -0500

    Increase AI power

--- bot/BotMain.java
+++ bot/BotMain.java
@@ -17,7 +17,7 @@ public class BotMain {
     public Tools.Side botSide;
 
     public final static int layers = 4;
-    public final static int movesPerLayer = 7;
+    public final static int movesPerLayer = 10;
     // int[first move] = {int[] first instruction, int[] second instruction, {rating Bounds} }
     public final static int firstMoveCount = 15;
     public volatile Evaluator[] toRuns = new Evaluator[firstMoveCount];
